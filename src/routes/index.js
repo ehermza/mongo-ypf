d26d65dbb379d999b6996d41a0cef9f035bfd728
EHER.MZA <ehermza@yahoo.com>
2021-07-06 23:00:29 -0300

    form data submit to database mongodb, working ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,6 @@ const Carga = require('../models/Carga');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    // res.send('hello world');
-    // res.render('index');
     const t_cargas = await Carga.find();
     res.render('index', { t_cargas });
 });
@@ -17,11 +15,6 @@ router.post('/add', async (req, res) => {
     console.log(req.body);
     const carga = new Carga(req.body);
     
-    // carga.to = 'Comercial Oeste sa';
-    // carga.litros_ch = 20.35;
-    // carga.price = 2000;
-    // carga.n_ticket= uuid();
-
     await carga.save();
     // carga.product = 'Infinia'
     res.redirect('/');
